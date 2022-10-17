d89a94afe706b71b826c5a5a5bc79550549f383b
Andrew Fecton <bopongamer@gmail.com>
2022-10-17 16:55:15 +0430

    A lot of changes 1. Changed the background 2. Added links to presentation files 3. Made last modified via JS 4. Little corrections

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var header1 = document.getElementById("header1");
+var last_modified = document.getElementById("last_modified")
+
 var i = 0;
 var direction = -1;
 var arr = [2,4,6,8,10,12,14];
 var length = arr.length;
+
+var date = [17,10,2022];
+var months_str = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+if(date[1] < 0 || date[1] > 12){
+    alert("ERROR: BAD MONTH!!!");
+}
+else{
+    last_modified.textContent = "Last Updated: " + String(date[0]) + " " + String(months_str[date[1]]) + " " + String(date[2]); 
+}
+
+
 
 function TextSpacing(){
     header1.style.letterSpacing = String(arr[i]) + "px";
