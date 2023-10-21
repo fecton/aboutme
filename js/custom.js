7cd4a20c1dabfcfd32dc36bdcb5e450e0e892938
fecton <bopongamer@gmail.com>
2023-10-21 05:19:42 +0200

    Update content of webpage
    - Update profile photo
    - Update 'Skills' section
    - Refactor 'templatemo-blue.css'
    - Increase duration in slideshow

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,4 @@
-// templatemo 467 easy profile
-
 // PRELOADER
-
 $(window).load(function(){
     $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
 });
@@ -13,6 +10,6 @@ $(function(){
 	 		 "images/tm-bg-slide-1.jpg", 
 	 		 "images/tm-bg-slide-2.jpg",
 			 "images/tm-bg-slide-3.jpg"
-	 			], 	{duration: 9600, fade: 2600});
+	 			], 	{duration: 15000, fade: 2600});
 		});
 })
