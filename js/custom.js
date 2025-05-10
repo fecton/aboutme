97181ff0b3327baace2dadd9673d378db5491b7c
fecton <bopongamer@gmail.com>
2025-05-10 23:22:15 +0200

    feat: set static background

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,11 @@
 // PRELOADER
 $(window).load(function(){
-    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets
+    $('.preloader').delay(1000).fadeOut("slow");
 });
 
-// HOME BACKGROUND SLIDESHOW
+// Set static background image
 $(function(){
     jQuery(document).ready(function() {
-		$('body').backstretch([
-				"images/tm-bg-slide-1.jpg",
-				"images/tm-bg-slide-2.jpg"
-			], 	{duration: 15000, fade: 2600});
-		});
+		$('body').backstretch("images/tm-bg-slide-1.jpg")
+	});
 })
