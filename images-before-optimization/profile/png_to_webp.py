a705cd0638609d44e3ea0ab1dde710a623e39afe
Andrii Lytvynenko <a.v.lytvynenko2004@gmail.com>
2026-02-22 18:50:49 +0100

    feat: add PNG to WebP conversion script and new profile image

---
+++ images-before-optimization/profile/png_to_webp.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import os
+
+all_files = os.listdir(".")
+
+print("All files to convert:", all_files)
+
+all_files.remove("png_to_webp.py")
+
+for file in all_files:
+    img = Image.open(file)
+    img.save(f"{os.path.splitext(file)[0]}.webp", format="webp", quality=100)
